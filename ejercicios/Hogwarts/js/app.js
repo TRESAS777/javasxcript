fdfecf1b3dc53517b14a28ac6ca247745c915367
TRESAS777 <103174712+TRESAS777@users.noreply.github.com>
2023-12-01 13:32:30 -0500

    Add files via upload

---
+++ ejercicios/Hogwarts/js/app.js
@@ -0,0 +1,45 @@
+const estudianteDeHogwarts = {
+    Nombre, 
+    edad, 
+    familia, 
+    linaje, 
+    casa, 
+    animalPatronus,
+    cualidades,
+};
+
+const clases_Y_Profesores = {
+    transformaciones: "Profesor Kevin Slughorn",
+    herbologia: "Profesor Maria Umbridge",
+    pociones: "Profesor Liliana McGonagall",
+    encantamientos: "Profesora Jackie",
+    defensaContraLasArtesOscuras: "Profesor Robinson Snape ",
+    animalesMagicos: "Profesor David Filch",
+    historiaDeMagia: "Profesor Ronald Sprout",
+};
+
+const obtenerCasaAleatorea = {
+    Gryffindor: "Valor, fuerza, audacia",
+    Hufflepuff: "Justicia, Lealtad, Paciencia",
+    Ravenclaw: "Creatividad, Erudición, Inteligencia",
+    Slytherin: "Ambición, Determinación, Astucia"
+};
+
+const linaje = {
+    
+}
+
+function obtenerCasaAleatorea () {
+    const casaAleatorea = Math.round(Math.random() * 3)
+    switch(casaAleatorea) {
+        case 0: 
+            return (`Gryffindor: "Valor, fuerza, audacia", "Linaje: Mestizo, muggle, Sangre pura" `)
+        case 1:
+            return (`Hufflepuff: "Justicia, Lealtad, Paciencia" Linaje: Mestizo, muggle`)
+        case 2:
+            return (`Ravenclaw(“Creatividad, Erudición, Inteligencia" Linaje: Mestizo, muggle, Sangre pura)`)
+        case 3:
+        return (`Slytherin: "Ambición, Determinación, Astucia" Linaje: "Sangre Pura")`)
+    }
+
+};
