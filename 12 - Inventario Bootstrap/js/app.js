e18aed39b2e5d029c9b58d0164e936f731206d84
TRESAS777 <103174712+TRESAS777@users.noreply.github.com>
2023-12-12 09:32:01 -0500

    Add files via upload

---
+++ 12 - Inventario Bootstrap/js/app.js
@@ -0,0 +1,104 @@
+//Selectores
+const tbody = document.querySelector("#tbody_table  ");
+const btnAgregar = document.querySelector("#btn-agregar");
+
+// Eventos
+btnAgregar.addEventListener("click", (event) => {
+    event.preventDefault();
+    agregarProducto();
+});
+
+// Lista para guardar todos los productos 
+const listaProductos = [
+    { id: Date.now() ,nombre: "Pastas", precio: "5.0", cantidad: 10, imagen: "", categoria: "carbohidrato", }
+];
+
+// funcion para agregar un producto
+function agregarProducto() {
+    // selectores de los inputs y al mismp tiempo acceder al valor
+    const nombreText = document.querySelector("#nombre_producto").value;
+    const cantidadText = document.querySelector("#cantidad").value;
+    const precioText = document.querySelector("#precio").value;
+    const imageText = document.querySelector("#image").value;
+    const categoriaText = document.querySelector("#categoria").value;
+
+
+    console.log({nombreText, cantidadText, precioText, imageText, categoriaText});
+    
+    const alerta = document.querySelector("#alert");
+    alerta.classList = "alert alert-danger d-none"
+
+    if ([nombreText, cantidadText, precioText, categoriaText].includes("")) {
+        alerta.textContent = "Todos los campos son obligatorios.";
+        alerta.classList.remove("d-none");
+        return;
+    }
+
+    alerta.classList.add("d-none");
+
+    listaProductos.push( {id: Date.now(), nombre: nombreText, cantidad: cantidadText, precio: precioText, imagen: imageText, categoria: categoriaText});
+    
+    alerta.classList.remove("d-none");
+    alerta.classList.replace("alert-danger", "alert-success");
+    alerta.textContent = `El producto ${nombreText} fue agregado exitosamente.`
+
+    document.querySelector("#form-agregar").reset()
+
+    mostrarProductos();
+}
+
+
+
+// funcion para listar los productos y mostrarlos en la
+function mostrarProductos() {
+    //Limpiar la tabla
+    tbody.innerHTML = "";
+    /**Recorro mi lista 
+     * donde producto es el item que se esta iterando
+     * y index es el indice de la lista de ese item
+    */
+    listaProductos.forEach( (producto, index) => {
+        /**Desestructurar el objeto que se esta recorriendo */
+        const { cantidad, categoria, id, imagen, nombre, precio } = producto
+        /**Dar formato al precio */
+        const precioFormat = Number(precio).toLocaleString( "en-US", {
+            style: "currency",
+            currency: "USD"
+        } );
+        /**Modifico el HTML del tbody */
+        tbody.innerHTML += `
+            <tr>
+                <td>${index+1}</td>
+                <td>
+                    <img src="${imagen}" alt="Imagen producto" width="40px" height="40px" class="rounded-circle"  >
+                </td>
+                <td>${nombre}</td>
+                <td>${precioFormat}</td>
+                <td>${categoria}</td>
+                <td>${cantidad}</td>
+
+                <td>
+                    <button
+                        class="btn btn-primary edit-product"
+                        data-id=${id}
+                    >
+                        Editar 
+                        <i class='bx bxs-edit'></i>
+                        <!-- icono Editar -->
+                    </button>
+
+                    <button 
+                        class="btn btn-danger delete-product" 
+                        data-id=${id}
+                    >
+                        Eliminar 
+                        <i class='bx bxs-trash'></i>
+                        <!-- icono Eliminar -->
+                    </button>
+                </td>
+            </tr>
+        `;
+    });
+};
+
+mostrarProductos();
